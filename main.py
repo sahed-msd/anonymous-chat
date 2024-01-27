1ac4788d0c4640e6033dbe39c2120970b9724804
sahed-msd <145775384+sahed-msd@users.noreply.github.com>
2024-01-28 04:18:46 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,61 @@
+import streamlit as st
+import os
+
+def save_data(data):
+    with open("saved_text.txt", "a", encoding="utf-8") as file:
+        file.write(data + "\n")
+
+def load_text():
+    if os.path.exists("saved_text.txt"):
+        with open("saved_text.txt", "r", encoding="utf-8") as file:
+            return file.readlines()[::-1]
+    return []
+
+def delete_all_data():
+    if os.path.exists("saved_text.txt"):
+        os.remove("saved_text.txt")
+
+def main():
+    st.title("Aɴᴏɴʏᴍᴏᴜs ᴄʜᴀᴛ")
+
+    # Text input area for writing
+    text_input = st.text_area("𝗪𝗿𝗶𝘁𝗲 𝘀𝗼𝗺𝗲𝘁𝗵𝗶𝗻𝗴:", key="text_input")
+
+    # Button to save text
+    save_button = st.button("𝙎𝙖𝙣𝙙 𝙢𝙚𝙨𝙨𝙖𝙜𝙚")
+    
+    # Manual refresh button
+    if st.button("𝗥𝗲𝗳𝗿𝗲𝘀𝗵"):
+        st.experimental_rerun()
+
+
+
+    # Delete all entries button
+    show_delete_button = st.checkbox("Show Delete Button")
+
+    # Delete all entries button
+    if show_delete_button and st.button("𝘿𝙚𝙡𝙚𝙩𝙚 𝙀𝙫𝙚𝙧𝙮𝙩𝙝𝙞𝙣𝙜"):
+        delete_all_data()
+        st.success("Deleted Successfully!")
+
+    # Display the written text
+    if save_button and text_input:
+        save_data(text_input)
+        st.success("𝗠𝗲𝘀𝘀𝗮𝗴𝗲 𝗦𝗲𝗻𝗱")
+
+    # Display the saved text
+    saved_text = load_text()
+    if saved_text:
+        st.subheader("𝘾𝙝𝙖𝙩 𝙃𝙞𝙨𝙩𝙤𝙧𝙮")
+        text_display = st.empty()
+        text_display.write(saved_text)
+
+    # Auto-refresh the page every 5 seconds (for demonstration purposes)
+    st.markdown(
+        """<meta http-equiv="refresh" content="1000">""",
+        unsafe_allow_html=True,
+    )
+
+
+if __name__ == "__main__":
+    main()
